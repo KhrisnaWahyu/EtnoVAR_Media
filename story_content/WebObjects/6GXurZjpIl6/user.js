a526662c0590a38c477d8d5314aecab3fa27bb59
Khrisna Wahyu Wibisono <145247951+KhrisnaWahyu@users.noreply.github.com>
2025-10-29 10:38:39 +0700

    Rev-01

--- story_content/WebObjects/6GXurZjpIl6/user.js
+++ story_content/WebObjects/6GXurZjpIl6/user.js
@@ -2,29 +2,23 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6o2fBQ2Q7cs":
+      case "6FogH2q6yS4":
         Script1();
         break;
-      case "6qoaKSo4R3i":
+      case "5uPGoKjM8v0":
         Script2();
         break;
-      case "5oUofvFv7Cq":
+      case "6FKdL5CqQDU":
         Script3();
         break;
-      case "646KjC1JSjL":
+      case "6NHUTKwzejn":
         Script4();
         break;
-      case "662crqUmQAZ":
+      case "5hDMcbNCPPc":
         Script5();
         break;
-      case "6I17R2U5Ni3":
+      case "64AWTvkzkod":
         Script6();
-        break;
-      case "6oJsqDavbzg":
-        Script7();
-        break;
-      case "65VQr9lvoTh":
-        Script8();
         break;
   }
 }
@@ -305,116 +299,6 @@ function Script5()
 }
 
 function Script6()
-{
-  (function() {
-    var elem = document.documentElement;
-    var player = GetPlayer();
-
-    // Fungsi masuk fullscreen + kunci landscape
-    function enterFullscreenAndLock() {
-        var request = elem.requestFullscreen || elem.webkitRequestFullscreen || elem.msRequestFullscreen;
-        if (request) {
-            request.call(elem).then(() => {
-                // Setelah fullscreen, kunci ke landscape
-                if (screen.orientation && screen.orientation.lock) {
-                    screen.orientation.lock("landscape").catch(err => {
-                        console.log("Tidak bisa mengunci orientasi:", err);
-                    });
-                }
-            }).catch(err => console.log("Gagal fullscreen:", err));
-        }
-    }
-
-    // Fungsi keluar fullscreen + kunci portrait
-    function exitFullscreenAndUnlock() {
-        var exit = document.exitFullscreen || document.webkitExitFullscreen || document.msExitFullscreen;
-        if (exit) {
-            exit.call(document).then(() => {
-                // Kembali ke portrait setelah keluar fullscreen
-                if (screen.orientation) {
-                    try {
-                        if (screen.orientation.unlock) {
-                            screen.orientation.unlock();
-                        }
-                        if (screen.orientation.lock) {
-                            screen.orientation.lock("portrait").catch(()=>{});
-                        }
-                    } catch(e) {
-                        console.log("Tidak bisa mengubah orientasi:", e);
-                    }
-                }
-            }).catch(err => console.log("Gagal keluar fullscreen:", err));
-        }
-    }
-
-    // Toggle fullscreen on/off
-    if (!document.fullscreenElement && !document.webkitFullscreenElement) {
-        enterFullscreenAndLock();
-        player.SetVar("isFull", true);
-    } else {
-        exitFullscreenAndUnlock();
-        player.SetVar("isFull", false);
-    }
-})();
-
-}
-
-function Script7()
-{
-  (function() {
-    var elem = document.documentElement;
-    var player = GetPlayer();
-
-    // Fungsi masuk fullscreen + kunci landscape
-    function enterFullscreenAndLock() {
-        var request = elem.requestFullscreen || elem.webkitRequestFullscreen || elem.msRequestFullscreen;
-        if (request) {
-            request.call(elem).then(() => {
-                // Setelah fullscreen, kunci ke landscape
-                if (screen.orientation && screen.orientation.lock) {
-                    screen.orientation.lock("landscape").catch(err => {
-                        console.log("Tidak bisa mengunci orientasi:", err);
-                    });
-                }
-            }).catch(err => console.log("Gagal fullscreen:", err));
-        }
-    }
-
-    // Fungsi keluar fullscreen + kunci portrait
-    function exitFullscreenAndUnlock() {
-        var exit = document.exitFullscreen || document.webkitExitFullscreen || document.msExitFullscreen;
-        if (exit) {
-            exit.call(document).then(() => {
-                // Kembali ke portrait setelah keluar fullscreen
-                if (screen.orientation) {
-                    try {
-                        if (screen.orientation.unlock) {
-                            screen.orientation.unlock();
-                        }
-                        if (screen.orientation.lock) {
-                            screen.orientation.lock("portrait").catch(()=>{});
-                        }
-                    } catch(e) {
-                        console.log("Tidak bisa mengubah orientasi:", e);
-                    }
-                }
-            }).catch(err => console.log("Gagal keluar fullscreen:", err));
-        }
-    }
-
-    // Toggle fullscreen on/off
-    if (!document.fullscreenElement && !document.webkitFullscreenElement) {
-        enterFullscreenAndLock();
-        player.SetVar("isFull", true);
-    } else {
-        exitFullscreenAndUnlock();
-        player.SetVar("isFull", false);
-    }
-})();
-
-}
-
-function Script8()
 {
   (function() {
     var elem = document.documentElement;
