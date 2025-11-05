b63cb78629f55dfc11ac5848dd94fe948eeecbfe
Khrisna Wahyu Wibisono <145247951+KhrisnaWahyu@users.noreply.github.com>
2025-11-05 17:17:28 +0700

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,419 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6JLD6Yl8eWJ":
+        Script1();
+        break;
+      case "6WzdWuq3Su4":
+        Script2();
+        break;
+      case "64A43jOU1Tj":
+        Script3();
+        break;
+      case "6cfCGezjlv4":
+        Script4();
+        break;
+      case "6icjCnOxuZi":
+        Script5();
+        break;
+      case "5V7sglB1JFy":
+        Script6();
+        break;
+      case "6QVoAFlqHAU":
+        Script7();
+        break;
+      case "5it45wgrzNc":
+        Script8();
+        break;
+      case "6460s5AUneK":
+        Script9();
+        break;
+      case "6c9RV1RP6TC":
+        Script10();
+        break;
+      case "6hWzRljpdzA":
+        Script11();
+        break;
+      case "6mFe5uHnGB9":
+        Script12();
+        break;
+      case "5lc1Q4txkip":
+        Script13();
+        break;
+      case "6W6UyTFCDc1":
+        Script14();
+        break;
+      case "6Es7o4lRqtX":
+        Script15();
+        break;
+      case "5lmbbWZf2I7":
+        Script16();
+        break;
+      case "5Wnmi4wOExn":
+        Script17();
+        break;
+      case "5zuUZ0OnzWA":
+        Script18();
+        break;
+      case "5eGR7oRCkal":
+        Script19();
+        break;
+      case "6jand1NzV7t":
+        Script20();
+        break;
+      case "6VeBFntZaEb":
+        Script21();
+        break;
+      case "5dawBnlfbB3":
+        Script22();
+        break;
+      case "6DokWT9gcOY":
+        Script23();
+        break;
+      case "6IMCPdqQq8Q":
+        Script24();
+        break;
+      case "5XPSQKz9L72":
+        Script25();
+        break;
+      case "6WunMsQFfaK":
+        Script26();
+        break;
+      case "6KQKXtJXxCu":
+        Script27();
+        break;
+      case "5wZHJlqbdBh":
+        Script28();
+        break;
+      case "6ciMtS98o3b":
+        Script29();
+        break;
+      case "6PdPPn6Cqjt":
+        Script30();
+        break;
+      case "5aThMxWrIyX":
+        Script31();
+        break;
+      case "69lv5wiS046":
+        Script32();
+        break;
+      case "6NEGtUzPg58":
+        Script33();
+        break;
+      case "66Ij8jV2lKu":
+        Script34();
+        break;
+      case "5ndR5OzPfG7":
+        Script35();
+        break;
+      case "5ddGv9Fycz9":
+        Script36();
+        break;
+      case "5aZStqx7wpR":
+        Script37();
+        break;
+      case "5XHUDu6pXuu":
+        Script38();
+        break;
+      case "6TzRWkm7PLO":
+        Script39();
+        break;
+      case "61qBU0ZkvlZ":
+        Script40();
+        break;
+      case "5UokUPwtQWR":
+        Script41();
+        break;
+      case "6MVIDsq4n8H":
+        Script42();
+        break;
+      case "66hCbLeOHhM":
+        Script43();
+        break;
+      case "6V5i5PpY9Xe":
+        Script44();
+        break;
+      case "5ecsM4roGTp":
+        Script45();
+        break;
+      case "6IiH4oQJk3X":
+        Script46();
+        break;
+      case "5fFPJ98KrEw":
+        Script47();
+        break;
+      case "6cIYh3kjsgS":
+        Script48();
+        break;
+      case "5jRXTzCsnOx":
+        Script49();
+        break;
+      case "5ggRrhxfgw2":
+        Script50();
+        break;
+      case "6SGXDWuz303":
+        Script51();
+        break;
+      case "6Jv8sFrAXRP":
+        Script52();
+        break;
+      case "5f7Q8NfiajG":
+        Script53();
+        break;
+      case "6flPXQnR5Hq":
+        Script54();
+        break;
+      case "6IDbmJFmc5D":
+        Script55();
+        break;
+      case "6RdkWMHrrXN":
+        Script56();
+        break;
+      case "653HWwKLC8I":
+        Script57();
+        break;
+      case "5uvXkuURjuq":
+        Script58();
+        break;
+      case "5px2scHiMtO":
+        Script59();
+        break;
+      case "5h1kIufBgDr":
+        Script60();
+        break;
+      case "5y1NO3SnZJ8":
+        Script61();
+        break;
+      case "6Tiu4VQdiSK":
+        Script62();
+        break;
+      case "5WTieFUPJv9":
+        Script63();
+        break;
+      case "68BmQzwbmR0":
+        Script64();
+        break;
+      case "6k1aiHRv4p4":
+        Script65();
+        break;
+      case "6MKZbc4pBEc":
+        Script66();
+        break;
+      case "6KcN2Qp1Qdd":
+        Script67();
+        break;
+      case "6KIWUjPyW6B":
+        Script68();
+        break;
+      case "6ZutuWfNPNk":
+        Script69();
+        break;
+      case "6geKFG3pl0L":
+        Script70();
+        break;
+      case "5bqz5xLZzlB":
+        Script71();
+        break;
+      case "6XUvHGQ9HvT":
+        Script72();
+        break;
+      case "5wOoO2lQb0F":
+        Script73();
+        break;
+      case "6lNLLj417IT":
+        Script74();
+        break;
+      case "6OYOcM6AisK":
+        Script75();
+        break;
+      case "5rb6O4vHuGv":
+        Script76();
+        break;
+      case "5jEjkYE0rR1":
+        Script77();
+        break;
+      case "658yVkthx7v":
+        Script78();
+        break;
+      case "61hu0rGf6RU":
+        Script79();
+        break;
+      case "5cqAqvqesBM":
+        Script80();
+        break;
+      case "6bgMWOh5zt3":
+        Script81();
+        break;
+      case "5r25BP63Ihw":
+        Script82();
+        break;
+      case "6SnASmrCTP9":
+        Script83();
+        break;
+      case "5nahMUkWvK3":
+        Script84();
+        break;
+      case "64EE9sMDoLs":
+        Script85();
+        break;
+      case "6nEfk0IqVL5":
+        Script86();
+        break;
+      case "5z2FKFQOSup":
+        Script87();
+        break;
+      case "6DVutpyxRtK":
+        Script88();
+        break;
+      case "5sLK9pdaRaf":
+        Script89();
+        break;
+      case "64h2htaC2sG":
+        Script90();
+        break;
+      case "6Ew4jV5ne3T":
+        Script91();
+        break;
+      case "5kN7ToCA82u":
+        Script92();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  player.once(() => {
+const target = object('5qpSjYyjqSy');
+const duration = 6000;
+const easing = 'ease-out';
+const id = '5hh4xAJy6ca';
+const teeterAmount = 4;
+const delay = 0;
+addToTimeline(
+target.animate(
+[ {rotate: '0deg' }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: `${teeterAmount}deg` }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: '0deg' } ]
+,
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script2 = function()
+{
+  player.once(() => {
+const target = object('5WsZMmE00dY');
+const duration = 750;
+const easing = 'linear';
+const id = '5qjg5YLpSpT';
+const growAmount = 0;
+const delay = 0;
+addToTimeline(
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script3 = function()
+{
+  const target = object('5WsZMmE00dY');
+const duration = 750;
+const easing = 'linear';
+const id = '5qjg5YLpSpT';
+const growAmount = 0;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script4 = function()
+{
+  const target = object('5WsZMmE00dY');
+const duration = 750;
+const easing = 'linear';
+const id = '6LbnF1oYNzP';
+const shrinkAmount = 0.1;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: `${1 - shrinkAmount}` } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script5 = function()
+{
+  const target = object('5qpSjYyjqSy');
+const duration = 6000;
+const easing = 'ease-out';
+const id = '5hh4xAJy6ca';
+const teeterAmount = 4;
+player.addForTriggers(
+id,
+target.animate(
+[ {rotate: '0deg' }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: `${teeterAmount}deg` }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: '0deg' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script6 = function()
+{
+  player.once(() => {
+const target = object('5qpSjYyjqSy');
+const duration = 6000;
+const easing = 'ease-out';
+const id = '5hh4xAJy6ca';
+const teeterAmount = 4;
+const delay = 0;
+addToTimeline(
+target.animate(
+[ {rotate: '0deg' }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: `${teeterAmount}deg` }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: '0deg' } ]
+,
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+};
